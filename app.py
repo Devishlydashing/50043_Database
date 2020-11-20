5c3109281ef730757d85df9467b8510970646875
leeminhan <minhan.lmh@gmail.com>
2020-11-21 01:11:09 +0800

    Completed fixes on all end points and updated POST

--- app.py
+++ app.py
@@ -6,15 +6,12 @@ from books.resource import Review, ReviewList
 app = Flask(__name__)
 db.init_app(app)
 
-# replace localhost: ip_address (for ec2), db_name: database name
-# DATABSE_URI='mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='password', server='localhost', database='50043_DB')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/50043_db' # DATABSE_URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/50043_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # This turns off FlaskSQLAlchemy modification tracker but not SQLAlchemy modification tracker
 api = Api(app)
 
-api.add_resource(Review, '/review/<id>')
-api.add_resource(ReviewList, '/reviews/<string:asin>')
+api.add_resource(Review, '/review/')
+api.add_resource(ReviewList, '/reviews/')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
