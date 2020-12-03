56622fe30b4c9d75ac8830b673147b7f3bed07ae
leeminhan <minhan.lmh@gmail.com>
2020-12-03 23:56:33 +0800

    backbone of tfidf function

--- analytics/spark_app.py
+++ analytics/spark_app.py
@@ -10,3 +10,15 @@ def show_df(df, n_show):
     rows = df.take(n_show)
     for r in rows:
         print(r)
+
+def tfidf_review_text(df):
+    df = df.select(["reviewText"]).dropna()
+
+    tokenizer = ml.feature.Tokenizer(inputCol="reviewText", outputCol="token")
+    hasher = ml.feature.CountVectorizer(inputCol="token", outputCol="hash")
+    idf = ml.feature.IDF(inputCol="hash", outputCol="tfidf")
+    pipeline = ml.Pipeline(stages=[tokenizer, hasher, idf])
+    pipeline = pipeline.fit(df)
+    df = pipeline.transform(df)
+    
+    return df
