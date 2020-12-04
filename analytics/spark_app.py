24e6212a255a593cb85750dff599bd6759be49ee
leeminhan <minhan.lmh@gmail.com>
2020-12-04 15:31:58 +0800

    create Timer & update tfidf with model

--- analytics/spark_app.py
+++ analytics/spark_app.py
@@ -6,19 +6,40 @@ from pyspark.sql import SparkSession, utils
 from pyspark.sql.functions import udf
 from pyspark.sql import types
 
-def show_df(df, n_show):
-    rows = df.take(n_show)
+class Timer(object):
+    def __init__(self, name, decimals=1):
+        self.name = name
+        self.decimals = decimals
+
+    def __enter__(self):
+        self.start = time.time()
+        print("Start timer:", self.name)
+        
+    def __exit__(self, type, value, traceback):
+        duration = time.time() - self.start
+        duration = round(duration, self.decimals)
+        print("Ended timer: {}: {} s".format(self.name, duration))
+
+def show_df(df, no_rows_to_show):
+    rows = df.take(no_rows_to_show)
     for r in rows:
         print(r)
 
 def tfidf_review_text(df):
-    df = df.select(["reviewText"]).dropna()
+    with Timer("TF-IDF for reviewText"):
+        df = df.select(["reviewText"]).dropna()
 
-    tokenizer = ml.feature.Tokenizer(inputCol="reviewText", outputCol="token")
-    hasher = ml.feature.CountVectorizer(inputCol="token", outputCol="hash")
-    idf = ml.feature.IDF(inputCol="hash", outputCol="tfidf")
-    pipeline = ml.Pipeline(stages=[tokenizer, hasher, idf])
-    pipeline = pipeline.fit(df)
-    df = pipeline.transform(df)
-    
+    with Timer("TF-IDF pipeline"):
+        tokenizer = ml.feature.Tokenizer(inputCol="reviewText", outputCol="token")
+        hasher = ml.feature.CountVectorizer(inputCol="token", outputCol="hash")
+        idf = ml.feature.IDF(inputCol="hash", outputCol="tfidf")
+        pipeline = ml.Pipeline(stages=[tokenizer, hasher, idf])
+        model = pipeline.fit(df)
+        df = model.transform(df)
+
     return df
+
+if __name__ == "__main__":
+    
+    with Timer("My spark script"):
+        print("Running spark_app.py")
